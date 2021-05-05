f483d671cb02e72f2a81b603d55a10dfccdee261
erika-estobero <erika.estobero@cvsu.edu.ph>
2021-05-06 01:39:44 +0800

    first backup

---
+++ wp-content/themes/blank/contact-template.php
@@ -0,0 +1,21 @@
+<?php
+/* Template Name: Contact */
+get_header();
+?>
+<div>
+<center>
+<div class="contact-section">
+<b>	<h1>CONTACT US</h1></b>
+<b><p>GET IN TOUCH</p>
+<p>You are important to us!</p></b>
+
+<b><p>Address:</b> 0914 Sitio Kawayan Habay II, Bacoor City Cavite</p>
+<b><p>Contact:</b> Globe: 095-229-2374/TNT: 0930-842-7569</p>
+<b><p>Working Time:</b> 8:00am - 5:00pm</p>
+
+<b><p>Facebook:</b> BrilliantKikang</p>
+<b><p>Twitter:</b> BrilliantKikangPH</p>
+
+</div></center>
+
+<?php get_footer(); ?> 
---
+++ wp-content/themes/blank/home-template.php
@@ -0,0 +1,66 @@
+<?php 
+	/*Template Name: HOME */
+	get_header();
+
+ ?>
+<div class="container">
+
+  <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-80" src="http://localhost/wordpress/wp-content/uploads/2021/05/bg2-1.jpg" alt="First slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-80" src="http://localhost/wordpress/wp-content/uploads/2021/05/i6-1.jpg" alt="Second slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-80" src="http://localhost/wordpress/wp-content/uploads/2021/05/bg1.jpg" alt="Third slide">
+    </div>
+  </div>
+
+  <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="sr-only">Previous</span>
+  </a>
+  <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="sr-only">Next</span>
+  </a>
+  </div>
+
+    <div class="container">
+  <div class="row">
+    <div class="col-2">
+    <b>  <h1>  Welcome to <br> BOICEMY!</h1>
+      <p>" Start happiness with taking care of yourself"</p> 
+      <a href="" class="btn">View More &#8594;</a></b>
+    </div>
+  </div>
+<!--second-->
+ <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+  <ol class="carousel-indicators">
+    <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+    <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+  </ol>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img class="d-block w-80" src="http://localhost/wordpress/wp-content/uploads/2021/05/j3.jpg" alt="First slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-80" src="http://localhost/wordpress/wp-content/uploads/2021/05/h4.jpg" alt="Second slide">
+    </div>
+    <div class="carousel-item">
+      <img class="d-block w-80" src="http://localhost/wordpress/wp-content/uploads/2021/05/g1.jpg" alt="Third slide">
+
+    </div>
+  </div> 
+
+
+ <?php get_footer(); ?>
+ 
